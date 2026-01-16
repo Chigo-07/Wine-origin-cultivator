a5116895e3978b244ac383d412deb3a8e86ecb86
Chigo-07 <nkemerechigozirim14@gmail.com>
2026-01-16 05:34:34 +0100

    Initial commit - cultivator prediction system

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+
+with open("model.h5", "rb") as f:
+    model, scaler = pickle.load(f)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    result = None
+    if request.method == "POST":
+        values = [float(x) for x in request.form.values()]
+        values = scaler.transform([values])
+        result = model.predict(values)[0]
+
+    return render_template("index.html", result=result)
+
+app.run(debug=True)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
